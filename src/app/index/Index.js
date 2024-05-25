2f20f99a556f910cddde89b68ea52cf02be2ecf8
Washington <washington.contato1327@gmail.com>
2024-05-25 10:29:26 -0300

    Criação de cmponente de menu mobile

--- src/app/index/Index.js
+++ src/app/index/Index.js
@@ -5,17 +5,20 @@ import Inicio from "../components/Inicio/Inicio";
 import Projetos from "../components/Projetos/Projetos";
 import Contatos from "../components/Contatos/Contatos";
 import Ferramentas from "../components/Ferramentas/Ferramentas";
+import MenuMobile from "../components/MenuMobile/MenuMobile";
 
 
 export default function Index() {
     return (
         <div className={Styles.container}>
+            <MenuMobile />
             <Header />
             <Inicio />
             <Sobre />
             <Projetos />
             <Ferramentas />
             <Contatos />
+
         </div>
     )
 }
