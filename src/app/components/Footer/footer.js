9a7328505bd18564b80e37a65c70633be8fd602f
Washington <washington.contato1327@gmail.com>
2024-06-13 11:34:11 -0300

    Ajustes em funcionalidades de links

--- src/app/components/Footer/footer.js
+++ src/app/components/Footer/footer.js
@@ -3,7 +3,7 @@ import Styles from "./footer.module.css"
 export default function Footer() {
     return (
         <footer className={Styles.footer}>
-            <a href="https://www.linkedin.com/in/washington-lemos-033177186/" target="blank">@WashingtonLemos</a>
+            <a href="https://www.linkedin.com/in/washington-lemos-033177186/" target="blank">© 2024 Washington Lemos. Todos os direitos reservados.</a>
         </footer>
     )
 }
